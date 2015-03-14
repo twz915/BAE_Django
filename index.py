33e504c85a5fbefc873d125b16e403392e80c474
WeizhongTu <tuweizhong@163.com>
2015-03-14 21:58:05 +0800

    修复Django 1.7在BAE中部署的问题

--- index.py
+++ index.py
@@ -5,19 +5,8 @@
 # @Link    : http://www.tuweizhong.com
 
 import os
-import sys
+os.environ['DJANGO_SETTINGS_MODULE'] = 'zqxt.settings'
 
-# 这里修改为您的项目名称，其它的不要变
-PROJECT_NAME = 'zqxt'
-
-
-os.environ['DJANGO_SETTINGS_MODULE'] = '%s.settings'%PROJECT_NAME
-
-path = os.path.dirname(os.path.abspath(__file__)) + '/%s'%PROJECT_NAME
-if path not in sys.path:
-    sys.path.insert(1, path)
-
-from django.core.handlers.wsgi import WSGIHandler
+from django.core.wsgi import get_wsgi_application
 from bae.core.wsgi import WSGIApplication
- 
-application = WSGIApplication(WSGIHandler())
+application = WSGIApplication(get_wsgi_application())
